c16a2c3de2ec7668841a51c449cca87b817d0aae
4cruzeta <4cruzetam@gmail.com>
2020-01-05 16:50:21 -0300

    Clientes inserindo com Post

--- src/main/java/br/com/zentic/cursomc/resources/ClienteResource.java
+++ src/main/java/br/com/zentic/cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package br.com.zentic.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.zentic.cursomc.domain.Cliente;
 import br.com.zentic.cursomc.dto.ClienteDTO;
+import br.com.zentic.cursomc.dto.ClienteNewDTO;
 import br.com.zentic.cursomc.services.ClienteService;
 
 @RestController
@@ -41,6 +44,15 @@ public class ClienteResource {
 		return ResponseEntity.noContent().build();
 	}
 
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);

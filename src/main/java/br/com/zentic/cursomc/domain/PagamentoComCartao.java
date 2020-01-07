16a10fe0a227c788508a8ab3f0423a79242c6928
4cruzeta <4cruzetam@gmail.com>
2020-01-07 12:43:38 -0300

    Pedido Inserindo

--- src/main/java/br/com/zentic/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/br/com/zentic/cursomc/domain/PagamentoComCartao.java
@@ -2,9 +2,12 @@ package br.com.zentic.cursomc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
 import br.com.zentic.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 

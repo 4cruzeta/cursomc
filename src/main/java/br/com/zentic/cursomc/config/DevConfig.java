d2800ae1b8b70571bee1b7247b64eb2cdf08606e
4cruzeta <4cruzetam@gmail.com>
2020-01-08 12:10:18 -0300

    Pedido - Envia E-mail Real

--- src/main/java/br/com/zentic/cursomc/config/DevConfig.java
+++ src/main/java/br/com/zentic/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import br.com.zentic.cursomc.services.DBService;
+import br.com.zentic.cursomc.services.EmailService;
+import br.com.zentic.cursomc.services.SmtpEmailService;
 
 
 
@@ -31,5 +33,10 @@ public class DevConfig {
 
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }

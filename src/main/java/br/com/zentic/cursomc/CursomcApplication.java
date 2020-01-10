7aa06a0a1caeaf6c6f792eb4ee9853858f4294f1
4cruzeta <4cruzetam@gmail.com>
2020-01-10 14:27:14 -0300

    S3 - enviando imagem via endpoint

--- src/main/java/br/com/zentic/cursomc/CursomcApplication.java
+++ src/main/java/br/com/zentic/cursomc/CursomcApplication.java
@@ -1,17 +1,13 @@
 package br.com.zentic.cursomc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.zentic.cursomc.services.S3Service;
-
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 	
-	@Autowired
-	private S3Service s3Service;
+
 
 
 	public static void main(String[] args) {
@@ -20,7 +16,7 @@ public class CursomcApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {	
-		s3Service.uploadFile("/Volumes/1TB/Temp/3X4.jpg");
+
 			
 		
 	}
